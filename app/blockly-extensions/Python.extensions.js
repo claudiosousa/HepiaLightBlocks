b863aa29e056c999121626e2cb900e391465e090
claudio@aspire <claudio.sousa@gmail.com>
2018-10-11 19:03:01 +0200

    python generated code cleanup

---
+++ app/blockly-extensions/Python.extensions.js
@@ -0,0 +1,18 @@
+const finishFn = Blockly.Python.finish;
+
+Blockly.Python.finish = code => {
+    if (Blockly.Python.definitions_) {
+        delete Blockly.Python.definitions_['from_numbers_import_Number'];
+        delete Blockly.Python.definitions_['variables'];
+    }
+
+    return finishFn(code);
+}
+
+Blockly.Python.math_change = block => {
+    const argument0 = Blockly.Python.valueToCode(block, 'DELTA',
+        Blockly.Python.ORDER_ADDITIVE) || '0';
+    const varName = Blockly.Python.variableDB_.getName(block.getFieldValue('VAR'),
+        Blockly.Variables.NAME_TYPE);
+    return `${varName} = ${varName} + ${argument0}\n`;
+};
--- app/blockly-extensions/index.js
+++ app/blockly-extensions/index.js
@@ -1,1 +1,2 @@
 import './blocks/index.js';
+import './Python.extensions.js';
